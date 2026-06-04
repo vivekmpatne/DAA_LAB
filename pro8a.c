530c922b8c10c4b255d0ba901548cc35756ef4c9
vivekmpatne <vivekpatnem@gmail.com>
2026-06-04 22:34:59 +0530

    added content in pro8a(Floyd’s Algorithm)

---
+++ pro8a.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+#define MAX 20
+#define INF 9999
+
+int main() {
+
+    int n;
+    int dist[MAX][MAX];
+
+    printf("Enter number of cities: ");
+    scanf("%d", &n);
+
+    printf("Enter distance matrix:\n");
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &dist[i][j]);
+        }
+    }
+
+    for(int k = 0; k < n; k++) {
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    printf("\nShortest Path Matrix:\n");
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+
+            if(dist[i][j] == INF)
+                printf("INF ");
+
+            else
+                printf("%d ", dist[i][j]);
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
